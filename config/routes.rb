39c5fafce35a7849382c1bcbad32999e326ce289
Dmitry Samoylov <dmitry-samoilov.l@yandex.ru>
2020-10-06 21:41:39 +0300

    Result of work in the lesson

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
+
+  namespace :admin do
+    root 'welcome#index'
+  end
+  
+  resources :users 
+
+  resources :orders do
+  
+    member do
+      get 'approve'
+      get 'calc'
+    end
+  
+    collection do
+      get 'first'
+    end
+  
+  end
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
