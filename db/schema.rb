7d4e016d45c0904a3e01d96d20af79197ed33f67
Dmitry Samoilov <dmitry-samoilov.l@yandex.ru>
2020-10-13 17:28:13 +0300

    Added several changes made in the lesson on models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_06_170106) do
+ActiveRecord::Schema.define(version: 2020_10_13_141104) do
 
   create_table "orders", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,9 @@ ActiveRecord::Schema.define(version: 2020_10_06_170106) do
     t.integer "cost"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.json "options"
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -28,4 +31,5 @@ ActiveRecord::Schema.define(version: 2020_10_06_170106) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "orders", "users"
 end
