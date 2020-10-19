a441c1a77f517e24003889c8008ee4689279dfab
Dmitry Samoilov <dmitry-samoilov.l@yandex.ru>
2020-10-20 00:21:39 +0300

    Added first version of scope method for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,14 @@ class User < ApplicationRecord
   #                              message: "Первая буква должна быть заглавной" }
   # scope :users_with_created_orders, -> { select('user') }
 
+  def self.created_orders_low_to_hi
+    User.select(:name, 'SUM(cost) AS orders_sum')
+        .joins(:orders)
+        .group('users.name')
+        .where("status = '1'")
+        .order('orders_sum ASC')
+  end
+
   has_many :orders
   has_one :passport_data
 end
