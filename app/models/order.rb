845f154626e7f75642a0904095e18f99b2407874
Dmitry Samoilov <dmitry-samoilov.l@yandex.ru>
2020-10-20 13:42:35 +0300

    Fixed callbacks

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,14 +2,15 @@ class Order < ApplicationRecord
 
   # validates :name, length: { maximum: 10 }
 
-  # before_create :set_cost, on: [:update, :create]
+  before_create :set_cost
+  before_update :set_cost
 
   scope :high_cost, -> { where(cost: 1_000..) }
   scope :vip_failed, -> { failed.high_cost }
   scope :created_before, -> (time) { where('created_at < ?', time)}
 
   def set_cost
-    self.cost = rand(100)
+    self.cost = rand(1000)
   end
 
   enum status: %w[unavailable created started failed removed]
