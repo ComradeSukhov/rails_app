c1acde89e677058c0e7b48a86c82cea9422eb25d
Dmitry Samoilov <dmitry-samoilov.l@yandex.ru>
2020-10-27 13:21:03 +0300

    Removed unnecessary callbacks in Order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,15 +2,9 @@ class Order < ApplicationRecord
   
   validates :name, length: { maximum: 10 }
 
-  before_create :set_cost
-
   scope :high_cost, -> { where(cost: 1_000..) }
   scope :vip_failed, -> { failed.high_cost }
   scope :created_before, -> (time) { where('created_at < ?', time)}
-
-  def set_cost
-    self.cost = rand(1000)
-  end
 
   enum status: %w[unavailable created started failed removed]
 
